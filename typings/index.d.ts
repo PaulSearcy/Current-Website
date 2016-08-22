ed42bb0ba5b8dd654fd20ce54a57a374815f6ca0
tridon777 <tridon777@gmail.com>
2016-08-22 00:07:55 -0500

    Unstaging typing  modules

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/bootstrap/index.d.ts" />
-/// <reference path="globals/react-bootstrap/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
