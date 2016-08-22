c29d9e8d4e3c1e1b82294bf40c0cb2c64b53985b
tridon777 <tridon777@gmail.com>
2016-08-22 16:21:24 -0500

    Server Config
    -Adding express module
    -Adding server.js file for server startup

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const {resolve} = require('path');
+const fs = require('fs');
+const serveStatic = require('serve-static');
+const app = express();
+
+app.use('/dist', serveStatic(resolve(__dirname, './dist')));
+app.use('/src', serveStatic(resolve(__dirname, './src')));
+app.use('/stylesheets', serveStatic(resolve(__dirname, './stylesheets')));
+app.use('/node_modules', serveStatic(resolve(__dirname, './node_modules')));
+
+
+app.get('/', (req,res) => {
+    let options = { root: __dirname};
+    res.sendFile('./index.html',options);
+})
+
+let port = 7864;
+app.listen(port);
